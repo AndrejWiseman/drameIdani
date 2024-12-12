29323d893aad6f6ba30679a0bf9276341c904119
Andrej Wiseman <533737@gmail.com>
2024-12-12 12:03:05 +0100

    sitemap i robots

--- drameIdani/urls.py
+++ drameIdani/urls.py
@@ -7,18 +7,29 @@ from django.conf.urls.static import static
 
 from django.contrib.sitemaps.views import sitemap
 
-from home.sitemap import DrameSitemap
 from django.views.generic.base import TemplateView
 
+# from home.sitemap import DrameSitemap
+
+from .sitemap import StaticViewSitemap
+
+# sitemaps = {
+#     'home': DrameSitemap,
+# }
+
+
 sitemaps = {
-    'home': DrameSitemap,
+    'static': StaticViewSitemap,
 }
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 
     path('robots.txt',TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
